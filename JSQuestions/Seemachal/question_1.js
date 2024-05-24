2ec3ecdbf9699284ee306adf3a5a8586fc729f7d
seemachal-8848 <seemachal@8848digital.com>
2024-05-24 12:02:11 +0530

    Js question added for 24 May 2024 SG

---
+++ JSQuestions/Seemachal/question_1.js
@@ -0,0 +1,28 @@
+// Here, an array of friends is given. Each friend likes different books to read. Get all the books in one array and try to use a minimal line of code.
+
+let friends = [
+    {
+        name: 'Aman',
+        books: ['Harry Poter', 'Bible'],
+        age: 21
+    },
+    {
+        name: 'Bob',
+        books: ['War ans peace', 'Village Katha'],
+        age: 26
+    },
+    {
+        name: 'Rahul',
+        books: ['The Lord of the Rings', 'The Shining'],
+        age: 28
+    }
+]
+
+
+//output
+['Harry Poter', 
+'Bible','War ans peace', 
+'Village Katha',
+'The Lord of the Rings', 
+'The Shining'
+]
